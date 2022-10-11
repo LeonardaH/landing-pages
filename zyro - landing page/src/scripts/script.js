b5fa0ec7598f384e9cea1ee749f42a29b3d6c6bd
LeonardaH <93203715+LeonardaH@users.noreply.github.com>
2022-10-11 17:32:50 +0200

    Delete zyro - landing page

--- zyro - landing page/src/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".header__mobile__left__hamburger");
-const navList = document.querySelector(".header__mobile__left__nav");
-
-menu.addEventListener("click", () => {
-  menu.classList.toggle("change");
-  navList.classList.toggle("hide");
-  navList.classList.toggle("show");
-});
